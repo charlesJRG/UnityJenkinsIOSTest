c26dc8ae1e0f3620c49952e563035f942580abe3
Charles George <charles@jumprampgames.com>
2017-03-14 17:19:12 -0400

    GitIgnore stuff

---
+++ JenkinsTestProj/Assets/Editor/JenkinsFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class JenkinsFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static bool KickOffBuild(){
+		Debug.Log("I'm here");
+		return false;
+	}
+
+}
